f690685d33030c9c3f05bf9e1e45865c2c9529a0
abdallahsidibe <sidibeabdallah01@gmail.com>
2023-12-01 22:47:41 +0100

    animal

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import animal.Animal;
+import animal.Dog;
+import animal.Pig;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal myAnimal = new Animal();
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+    }
+}
---
+++ src/animal/Pig.java
@@ -0,0 +1,7 @@
+package animal;
+
+public class Pig extends Animal{
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
